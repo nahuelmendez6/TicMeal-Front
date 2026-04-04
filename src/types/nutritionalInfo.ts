2f2650e3dde8b313f86171f4189e4f33db8f4897
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-04 15:54:14 +0200

    feat(nutricional-info): Add the NutricionalInfor type. - This type provides the structure for the ingredients and items nutricional info

---
+++ src/types/nutritionalInfo.ts
@@ -0,0 +1,8 @@
+export interface NutritionalInfo {
+  calories: number; // por 100g o por unidad base
+  protein: number; // gramos
+  carbohydrates: number; // gramos
+  fat: number; // gramos
+  sugar?: number; // gramos
+  sodium?: number; // miligramos
+}
